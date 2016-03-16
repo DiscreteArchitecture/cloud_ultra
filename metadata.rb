3f4aea5432c34820dc9b8ac13cccb691f952b0e3
Lei He <lei.he@blackboard.com>
2016-03-16 13:30:01 +0800

    Initial.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'cloud_ultra'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures cloud_ultra'
+long_description 'Installs/Configures cloud_ultra'
+version '0.1.0'
